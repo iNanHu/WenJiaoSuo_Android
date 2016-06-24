4b8eaa668eab2026d7765b1dec3e6e73e5815b6f
JasonMiao <bagecelia@163.com>
2016-06-24 15:37:29 +0800

    框架搭建

--- app/src/main/java/com/inanhu/wenjiaosuo/SplashActivity.java
+++ app/src/main/java/com/inanhu/wenjiaosuo/SplashActivity.java
@@ -1,13 +1,32 @@
 package com.inanhu.wenjiaosuo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SplashActivity extends AppCompatActivity {
+
+    @BindView(R.id.ivAvatar)
+    ImageView ivAvatar;
+    @BindView(R.id.tvMessage)
+    TextView tvMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        tvMessage.setText("测试");
+    }
+
+    @OnClick(R.id.btnClick)
+    public void onClick() {
+        Toast.makeText(this, tvMessage.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
