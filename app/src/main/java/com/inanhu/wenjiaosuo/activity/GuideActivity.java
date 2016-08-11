0769be9d7fe3696a63e66268c82a6480daa0410b
JasonMiao <bagecelia@163.com>
2016-08-11 12:19:24 +0800

    新增引导页

---
+++ app/src/main/java/com/inanhu/wenjiaosuo/activity/GuideActivity.java
@@ -0,0 +1,13 @@
+package com.inanhu.wenjiaosuo.activity;
+
+import com.inanhu.wenjiaosuo.base.BaseActivity;
+
+/**
+ * 引导页
+ *
+ * Created by Jason on 2016/8/11.
+ */
+public class GuideActivity extends BaseActivity {
+
+
+}
